6f741afc66f84043e99f3d4a9fe8db3b309b2c49
Ishan Pradeep <ishan@fh.technology>
2023-08-18 23:01:26 +0530

    changes

--- app/Http/Resources/Task/TaskResource.php
+++ app/Http/Resources/Task/TaskResource.php
@@ -2,9 +2,12 @@
 
 namespace App\Http\Resources\Task;
 
+use App\Http\Resources\Employee\EmployeeResource;
+use App\Models\Designation\Designation;
 use App\Models\Employee\Employee;
 use App\Models\Project\Project;
 use App\Models\Task\TaskType;
+use App\Models\User\User;
 use Illuminate\Http\Resources\Json\JsonResource;
 
 class TaskResource extends JsonResource
@@ -19,14 +22,17 @@ class TaskResource extends JsonResource
      */
     public function toArray($request)
     {
+        $employee = Employee::find($this->employee_id);
         return [
             'id'=>$this->id,
             'date'=> $this->date,
             'task_detail'=> $this->task_detail,
             'number_of_hour'=> $this->number_of_hour,
             'comment'=> $this->comment,
-            'employee'=> Employee::find($this->employee_id),
-            'task_type'=> TaskType::find($this->task_type_id),
+            'user' => User::find($employee->user_id),
+            'employee'=> $employee,
+            "designation_name" => Designation::select('designation_name')->find($employee->designation_id),
+        'task_type'=> TaskType::find($this->task_type_id),
             'project' => Project::find($this->project_id)
         ];
     }
